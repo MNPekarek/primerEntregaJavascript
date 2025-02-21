044c1be611db43df0db60eceafa4c1d0bcf9dbff
matpek <matiaspekarek@gmail.com>
2025-02-21 20:29:03 -0300

    agregue los botones y sus eventos

--- script.js
+++ script.js
@@ -1,48 +1,81 @@
 
-let canciones =[
-    {nombre: "Perfect", cantante: "Ed Sheeran"},
-    {nombre: "Piel", cantante: "Thiago"}];
+let canciones = [
+    { nombre: "Perfect", cantante: "Ed Sheeran" },
+    { nombre: "Piel", cantante: "Thiago" }];
 
-function simulador(){
+// function simulador() {
 
-    let opcion;
+    // let opcion;
 
-    do {
-        opcion = prompt ("Ingrese una opción:\n 1: Mostrar canciones  \n 2: Agregar una cancion nueva \n 3: Buscar una canción \n 4: Cerrar");
+    //     do {
+    //         opcion = prompt ("Ingrese una opción:\n 1: Mostrar canciones  \n 2: Agregar una cancion nueva \n 3: Buscar una canción \n 4: Cerrar");
 
-        if (opcion === "1") {     
-            for(let objeto of canciones) {
-                alert(`Canción: ${objeto.nombre}    Cantante: ${objeto.cantante}`)                  
-            }
-        } else if (opcion === "2"){            
-            let cancionNueva = prompt("Ingrese el nombre de la cancion")
-            let artistaNuevo = prompt("Ingrese el nombre del autor")
-                 
-            if (cancionNueva !== null && artistaNuevo !== null) {
-                canciones.push({nombre: cancionNueva, cantante: artistaNuevo})  
-                alert("Canción agregada con exito")    
-            } else {
-                alert("Los datos ingresados no son válidos")
-            }
-        } else if (opcion === "3"){
+    //         if (opcion === "1") {     
+    //             for(let objeto of canciones) {
+    //                 alert(`Canción: ${objeto.nombre}    Cantante: ${objeto.cantante}`)                  
+    //             }
+            // } else if (opcion === "2"){            
+            //     let cancionNueva = prompt("Ingrese el nombre de la cancion")
+            //     let artistaNuevo = prompt("Ingrese el nombre del autor")
+
+            //     if (cancionNueva !== null && artistaNuevo !== null) {
+            //         canciones.push({nombre: cancionNueva, cantante: artistaNuevo})  
+            //         alert("Canción agregada con exito")    
+            //     } else {
+            //         alert("Los datos ingresados no son válidos")
+            //     }
+            // } else if (opcion === "3"){
+            //     let buscarCancion = prompt("Ingrese el nombre de la cancion que desea buscar").toUpperCase()
+
+            //     let cancionEncontrada = canciones.find((cancion) => cancion.nombre.toUpperCase() === buscarCancion);
+            //     if (cancionEncontrada) {
+            //         alert (`La busqueda se realizo con exito \n Nombre: ${cancionEncontrada.nombre}\nCantante: ${cancionEncontrada.cantante}`);
+            //     }         
+
+
+    //         } else if (opcion === "4"){
+    //             confirm("¿Estas seguro que quieres cerrar el simulador?")
+    //             alert("Simulador cerrado")        
+    //         } else {
+    //             alert("Opción inválida. Intente nuevamente")
+    //         }
+    //     }while (opcion !== "4");
+    // }
+
+    // simulador()
+
+    let btn1 = document.getElementById("button1")
+
+    btn1.addEventListener("click", () => {
+        for (let objeto of canciones) {
+        alert(`Canción: ${objeto.nombre}, Cantante: ${objeto.cantante}`)
+    }})
+
+
+
+    let btn2 = document.getElementById("button2")
+
+    btn2.addEventListener("click", () => {            
+        let cancionNueva = prompt("Ingrese el nombre de la cancion")
+        let artistaNuevo = prompt("Ingrese el nombre del autor")
+
+        if (cancionNueva !== null && artistaNuevo !== null) {
+            canciones.push({nombre: cancionNueva, cantante: artistaNuevo})  
+            alert("Canción agregada con exito")    
+        } else {
+            alert("Los datos ingresados no son válidos")
+        }})
+
+
+
+        btn3 = document.getElementById("button3")
+
+        btn3.addEventListener("click", () => {
             let buscarCancion = prompt("Ingrese el nombre de la cancion que desea buscar").toUpperCase()
 
-            let cancionEncontrada = canciones.find((cancion) => cancion.nombre.toUpperCase() === buscarCancion);
-            if (cancionEncontrada) {
-                alert (`La busqueda se realizo con exito \n Nombre: ${cancionEncontrada.nombre}\nCantante: ${cancionEncontrada.cantante}`);
-            }         
-
-               
-        } else if (opcion === "4"){
-            confirm("¿Estas seguro que quieres cerrar el simulador?")
-            alert("Simulador cerrado")        
-        } else {
-            alert("Opción inválida. Intente nuevamente")
-        }
-    }while (opcion !== "4");
-}
-
-simulador()
-
-
-
+                let cancionEncontrada = canciones.find((cancion) => cancion.nombre.toUpperCase() === buscarCancion);
+                if (cancionEncontrada) {
+                    alert (`La busqueda se realizo con exito \n Nombre: ${cancionEncontrada.nombre}\nCantante: ${cancionEncontrada.cantante}`);
+                }         
+        })
+    
